60b9682a9f7f4268ba437b46cd9de4c549305351
dhaneshawati <dawati66@gmail.com>
2023-11-08 00:25:43 +0530

    Added drag-drop of candies by implementing the valid moves logic

--- src/App.js
+++ src/App.js
@@ -18,12 +18,15 @@ const candyColors = [
 ];
 function App() {
   const [currentCandyArrangement, setCurrentCandyArrangement] = useState([]);
+  const [squareBeingDragged, setSquareBeingDragged] = useState(null);
+  const [squareBeingReplaced, setSquareBeingReplaced] = useState(null);
 
   useEffect(() => {
     createBoard();
   }, []);
 
-  const checkForColumnOfFive = () => {
+  const checkForColumnOfFive = (array) => {
+    let fiveColMatch = false;
     for (let i = 0; i < 60; i++) {
       const columnOfFive = [
         i,
@@ -32,36 +35,30 @@ function App() {
         i + width * 3,
         i + width * 4,
       ];
-      const decidedCandy = currentCandyArrangement[i];
+      const decidedCandy = array[i];
 
-      if (
-        columnOfFive.every(
-          (index) => currentCandyArrangement[index] === decidedCandy
-        )
-      ) {
-        columnOfFive.forEach(
-          (index) => (currentCandyArrangement[index] = blank)
-        );
+      if (columnOfFive.every((index) => array[index] === decidedCandy)) {
+        columnOfFive.forEach((index) => (array[index] = blank));
+        fiveColMatch = true;
       }
     }
+    return fiveColMatch;
   };
-  const checkForColumnOfFour = () => {
+  const checkForColumnOfFour = (array) => {
+    let fourColMatch = false;
     for (let i = 0; i < 69; i++) {
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
-      const decidedCandy = currentCandyArrangement[i];
+      const decidedCandy = array[i];
 
-      if (
-        columnOfFour.every(
-          (index) => currentCandyArrangement[index] === decidedCandy
-        )
-      ) {
-        columnOfFour.forEach(
-          (index) => (currentCandyArrangement[index] = blank)
-        );
+      if (columnOfFour.every((index) => array[index] === decidedCandy)) {
+        columnOfFour.forEach((index) => (array[index] = blank));
+        fourColMatch = true;
       }
     }
+    return fourColMatch;
   };
-  const checkForRowOfFive = () => {
+  const checkForRowOfFive = (array) => {
+    let fiveRowMatch = false;
     const notValid = [
       6, 7, 8, 9, 16, 17, 18, 19, 26, 27, 28, 29, 36, 37, 38, 39, 46, 47, 48,
       49, 56, 57, 58, 59, 66, 67, 68, 69, 76, 77, 78, 79, 86, 87, 88, 89, 96,
@@ -70,20 +67,19 @@ function App() {
 
     for (let i = 0; i < 100; i++) {
       const rowOfFive = [i, i + 1, i + 2, i + 3, i + 4];
-      const decidedCandy = currentCandyArrangement[i];
+      const decidedCandy = array[i];
 
       if (notValid.includes(i)) continue;
 
-      if (
-        rowOfFive.every(
-          (index) => currentCandyArrangement[index] === decidedCandy
-        )
-      ) {
-        rowOfFive.forEach((index) => (currentCandyArrangement[index] = blank));
+      if (rowOfFive.every((index) => array[index] === decidedCandy)) {
+        rowOfFive.forEach((index) => (array[index] = blank));
+        fiveRowMatch = true;
       }
     }
+    return fiveRowMatch;
   };
-  const checkForRowOfFour = () => {
+  const checkForRowOfFour = (array) => {
+    let fourRowMatch = false;
     const notValid = [
       7, 8, 9, 17, 18, 19, 27, 28, 29, 37, 38, 39, 47, 48, 49, 57, 58, 59, 67,
       68, 69, 77, 78, 79, 87, 88, 89, 97, 98, 99,
@@ -91,20 +87,19 @@ function App() {
 
     for (let i = 0; i < 100; i++) {
       const rowOfFour = [i, i + 1, i + 2, i + 3];
-      const decidedCandy = currentCandyArrangement[i];
+      const decidedCandy = array[i];
 
       if (notValid.includes(i)) continue;
 
-      if (
-        rowOfFour.every(
-          (index) => currentCandyArrangement[index] === decidedCandy
-        )
-      ) {
-        rowOfFour.forEach((index) => (currentCandyArrangement[index] = blank));
+      if (rowOfFour.every((index) => array[index] === decidedCandy)) {
+        rowOfFour.forEach((index) => (array[index] = blank));
+        fourRowMatch = true;
       }
     }
+    return fourRowMatch;
   };
-  const checkForRowOfThree = () => {
+  const checkForRowOfThree = (array) => {
+    let threeRowMatch = false;
     const notValid = [
       8, 9, 18, 19, 28, 29, 38, 39, 48, 49, 58, 59, 68, 69, 78, 79, 88, 89, 98,
       99,
@@ -112,34 +107,29 @@ function App() {
 
     for (let i = 0; i < 100; i++) {
       const rowOfThree = [i, i + 1, i + 2];
-      const decidedCandy = currentCandyArrangement[i];
+      const decidedCandy = array[i];
 
       if (notValid.includes(i)) continue;
 
-      if (
-        rowOfThree.every(
-          (index) => currentCandyArrangement[index] === decidedCandy
-        )
-      ) {
-        rowOfThree.forEach((index) => (currentCandyArrangement[index] = blank));
+      if (rowOfThree.every((index) => array[index] === decidedCandy)) {
+        rowOfThree.forEach((index) => (array[index] = blank));
+        threeRowMatch = true;
       }
     }
+    return threeRowMatch;
   };
-  const checkForColumnOfThree = () => {
+  const checkForColumnOfThree = (array) => {
+    let threeColMatch = false;
     for (let i = 0; i < 80; i++) {
       const columnOfThree = [i, i + width, i + width * 2];
-      const decidedCandy = currentCandyArrangement[i];
+      const decidedCandy = array[i];
 
-      if (
-        columnOfThree.every(
-          (index) => currentCandyArrangement[index] === decidedCandy
-        )
-      ) {
-        columnOfThree.forEach(
-          (index) => (currentCandyArrangement[index] = blank)
-        );
+      if (columnOfThree.every((index) => array[index] === decidedCandy)) {
+        columnOfThree.forEach((index) => (array[index] = blank));
+        threeColMatch = true;
       }
     }
+    return threeColMatch;
   };
   const moveIntoSquareBelow = () => {
     for (let i = 0; i < 100 - width; i++) {
@@ -162,19 +152,21 @@ function App() {
   };
   useEffect(() => {
     const timer = setInterval(() => {
-      checkForColumnOfFive();
-      checkForRowOfFive();
-      checkForColumnOfFour();
-      checkForRowOfFour();
-      checkForColumnOfThree();
-      checkForRowOfThree();
+      checkForColumnOfFive(currentCandyArrangement);
+      checkForRowOfFive(currentCandyArrangement);
+      checkForColumnOfFour(currentCandyArrangement);
+      checkForRowOfFour(currentCandyArrangement);
+      checkForColumnOfThree(currentCandyArrangement);
+      checkForRowOfThree(currentCandyArrangement);
       moveIntoSquareBelow();
       setCurrentCandyArrangement([...currentCandyArrangement]);
-      console.log("TESTING...");
+      // console.log("TESTING...");
     }, 100);
 
     return () => clearInterval(timer);
-  }, [currentCandyArrangement]);
+  }, [checkForColumnOfThree, checkForRowOfThree, currentCandyArrangement]);
+
+  const checkForMatching = () => {};
 
   const createBoard = () => {
     const randomCandyArrangement = [];
@@ -186,11 +178,105 @@ function App() {
     setCurrentCandyArrangement([...randomCandyArrangement]);
   };
   // console.log(currentCandyArrangement);
+  const dragStart = (e) => {
+    setSquareBeingDragged(e.target);
+    console.log("Drag start");
+  };
+  const dragDrop = (e) => {
+    setSquareBeingReplaced(e.target);
+    console.log("Drag drop");
+  };
+  const dragEnd = (e) => {
+    console.log("Drag end");
+    // replacing logic
+    const squareBeingDraggedId = parseInt(
+      squareBeingDragged.getAttribute("data-id")
+    );
+    const squareBeingReplacedId = parseInt(
+      squareBeingReplaced.getAttribute("data-id")
+    );
+    console.log(squareBeingReplacedId);
+    console.log(squareBeingDraggedId);
+    let dummyArr = [...currentCandyArrangement];
+    let swap = dummyArr[squareBeingReplacedId];
+    dummyArr[squareBeingReplacedId] = dummyArr[squareBeingDraggedId];
+    dummyArr[squareBeingDraggedId] = swap;
+
+    // Logic of valid movement
+    const validMoves = [
+      squareBeingDraggedId - width,
+      squareBeingDraggedId + 1,
+      squareBeingDraggedId + width,
+      squareBeingDraggedId - 1,
+    ];
+    console.log(squareBeingReplacedId);
+    // let swap = currentCandyArrangement[squareBeingReplacedId];
+    // currentCandyArrangement[squareBeingReplacedId] =
+    //   squareBeingDragged.getAttribute("src");
+    // currentCandyArrangement[squareBeingDraggedId] =
+    //   squareBeingReplaced.getAttribute("src");
+
+    const isMoveValid = validMoves.includes(squareBeingReplacedId);
+    const isAColumnOfFive = checkForColumnOfFive(dummyArr);
+    const isARowOfFive = checkForRowOfFive(dummyArr);
+    const isAColumnOfFour = checkForColumnOfFour(dummyArr);
+    const isARowOfFour = checkForRowOfFour(dummyArr);
+    const isAColumnOfThree = checkForColumnOfThree(dummyArr);
+    const isARowOfThree = checkForRowOfThree(dummyArr);
+    console.log("Move valid", isMoveValid);
+    console.log("5 column", isAColumnOfFive);
+    console.log("5 row", isARowOfFive);
+    console.log("4 column", isAColumnOfFour);
+    console.log("4 row", isARowOfFour);
+    console.log("3 column", isAColumnOfThree);
+    console.log("3 row", isARowOfThree);
+
+    if (
+      isMoveValid &&
+      (isAColumnOfFive ||
+        isARowOfFive ||
+        isAColumnOfFour ||
+        isARowOfFour ||
+        isAColumnOfThree ||
+        isARowOfThree)
+    ) {
+      // Move is valid
+      console.log("Inside if");
+      currentCandyArrangement[squareBeingReplacedId] =
+        squareBeingDragged.getAttribute("src");
+      currentCandyArrangement[squareBeingDraggedId] =
+        squareBeingReplaced.getAttribute("src");
+      setCurrentCandyArrangement([...currentCandyArrangement]);
+      setSquareBeingDragged(null);
+      setSquareBeingReplaced(null);
+    } else {
+      console.log("Inside else");
+      // Swaping candies back to their original position
+      // currentCandyArrangement[squareBeingDraggedId] =
+      //   squareBeingDragged.getAttribute("src");
+      // currentCandyArrangement[squareBeingReplacedId] =
+      //   squareBeingReplaced.getAttribute("src");
+      setCurrentCandyArrangement([...currentCandyArrangement]);
+    }
+  };
+
   return (
     <div className="App">
       <div className="game">
         {currentCandyArrangement.map((candy, index) => (
-          <img key={index} src={candy} alt="candy" data-id={index} />
+          <img
+            key={index}
+            src={candy}
+            alt="candy"
+            data-id={index}
+            draggable={true}
+            onDragStart={dragStart}
+            onDrop={dragDrop}
+            onDragEnd={dragEnd}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={(e) => e.preventDefault()}
+            onDragLeave={(e) => e.preventDefault()}
+          />
         ))}
       </div>
     </div>
